7f4524ac4370e46ff8d00d01b2bb1d2a664d0f9f
dupei <dupei.dp@gpus.arcslab.com>
2019-12-16 16:37:06 +0800

    fix compile error

--- dnnlib/tflib/custom_ops.py
+++ dnnlib/tflib/custom_ops.py
@@ -62,6 +62,8 @@ def _run_cmd(cmd):
 
 def _prepare_nvcc_cli(opts):
     cmd = 'nvcc ' + opts.strip()
+    cmd += ' --std c++11'
+    cmd += ' -DNDEBUG'
     cmd += ' --disable-warnings'
     cmd += ' --include-path "%s"' % tf.sysconfig.get_include()
     cmd += ' --include-path "%s"' % os.path.join(tf.sysconfig.get_include(), 'external', 'protobuf_archive', 'src')
